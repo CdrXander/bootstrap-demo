70b319e6cc9d24844f499a95a140a037d80b8b36
rnoelle <reidnoelle2@gmail.com>
2016-11-17 12:40:00 -0700

    well, jumbo, thumbnail, modal, row, btn

--- dataService.js
+++ dataService.js
@@ -1,32 +1,43 @@
 angular.module('boot')
-  .service('dataService', function () {
-    var movies = [{
-        title: 'Dark Knight',
-        rating: 4
+  .service('dataService', function ($http) {
+    var dogs = [{
+        id: 1,
+        name: 'Chachi',
+        picture: 'http://www.thelabradorsite.com/wp-content/uploads/2015/08/when-can-puppy-go-out.jpg',
+        likes: 1
       },
       {
-        title: 'Wait Until Dark',
-        rating: 5
+        id: 2,
+        name: 'Reginald',
+        picture: 'http://cdn1-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-2.jpg',
+        likes: 5
       },
       {
-        title: 'Inception',
-        rating: 4
+        id: 3,
+        name: 'Christopher Nolan',
+        picture: 'https://s-media-cache-ak0.pinimg.com/564x/29/fa/14/29fa1441f2a0446e9aa45cee74495a83.jpg',
+        likes: 0
       }
     ]
 
-    this.getMovies = function () {
-      var existingMovies = JSON.parse(localStorage.getItem('myMovies'));
-      if (!existingMovies) {
-        localStorage.setItem('myMovies', JSON.stringify(movies));
-        existingMovies = movies;
+    this.getDogs = function () {
+      return dogs;
+    }
+    this.addLike = function (id) {
+      for (var dog of dogs) {
+        if (dog.id == id) {
+          dog.likes++;
+          break;
+        }
       }
-      return existingMovies;
+      return dogs;
+    }
+    this.addPuppy = function (puppy) {
+      puppy.id = dogs.length + 1;
+      puppy.likes = 0;
+      dogs.push(puppy);
+      return dogs;
     }
 
-    this.addMovie = function (movie) {
-      var existingMovies = JSON.parse(localStorage.getItem('myMovies'));
-      existingMovies.push(movie);
-      localStorage.setItem('myMovies', JSON.stringify(existingMovies));
-      return existingMovies;
-    }
-  })
+
+})
--- mainCtrl.js
+++ mainCtrl.js
@@ -1,12 +1,21 @@
 angular.module('boot')
   .controller('mainCtrl', function ($scope, dataService) {
 
-    (function getMovies() {
-      $scope.movies = dataService.getMovies()
+    (function getDogs() {
+      $scope.dogs = dataService.getDogs()
+      dataService.getPuppyPics().then(function (response) {
+        $scope.puppypics = response;
+        console.log(puppypics);
+      })
     }())
 
 
-    $scope.addMovie = function(movie) {
-      $scope.movies = dataService.addMovie(movie)
+    $scope.addLike = function(id) {
+      $scope.dogs =dataService.addLike(id);
     }
-  })
+    $scope.addPuppy = function (puppy) {
+      $scope.dogs = dataService.addPuppy(puppy);
+    }
+
+
+})
